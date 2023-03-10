3dba8d0d7ff9f50c364c74ecaa75d3872372b7e2
EBRAHIM1018 <127453095+EBRAHIM1018@users.noreply.github.com>
2023-03-10 23:52:34 +0200

    Add files via upload

---
+++ NTI_AVR DRIVERS/NTI_AVR DRIVERS/Debug/makedep.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\KEYPAD\keypad.c
+
+HAL\LCD\lcd.c
+
+HAL\LCD\lcd_cfg.c
+
+HAL\STEPPER MOTOR\StepperMotor_config.c
+
+HAL\STEPPER MOTOR\StepperMotor_prog.c
+
+main.c
+
+MCAL\ADC\ADC_program.c
+
+MCAL\DIO\dio.c
+
+MCAL\EXTI0\EXTI0_Config.c
+
+MCAL\GIE\GIE_prog.c
+
+MCAL\SPI\SPI_prog.c
+
+MCAL\TIMER0\TIMR0_prg.c
+
+MCAL\TIMER1\TIMER1_prog.c
+
+MCAL\TIMER2\TIMR2_prg.c
+
